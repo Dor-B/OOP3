11b5d82df27132f5f2080ca248e3b089cefa4ac6
Dor-B <dor46200@gmail.com>
2020-05-25 14:22:01 +0300

    Add struct for dividing story into given, whens-thens,whens-thens, whens-thens ...

--- src/Solution/Main.java
+++ src/Solution/Main.java
@@ -3,6 +3,24 @@ package Solution;
 public class Main {
 
     public static void main(String[] args) {
+        StoryTesterImpl.StoryStruct s = new  StoryTesterImpl.StoryStruct(
+            "Given a classroom with a capacity of 50\n" +
+            "When the number of students in the classroom is 40\n" +
+            "When the number of broken chairs in the classroom is 10\n" +
+            "Then the classroom is full\n" +
+            "When the number of students in the classroom is 40\n" +
+            "Then the classroom is not-full"
+        );
+        System.out.println("GIVEN: " + s.givenSentence);
+        for(StoryTesterImpl.WhenThenStruct ws : s.whenThenGroups){
+            System.out.println("---------GROUP--------------");
+            System.out.println("+++++++++WHENS++++++++++++++");
+            for(String when :  ws.whens)
+                System.out.println(when);
+            System.out.println("+++++++++THENS++++++++++++++");
+            for(String then :  ws.thens)
+                System.out.println(then);
+        }
 	// write your code here
     }
 }
--- src/Solution/StoryTesterImpl.java
+++ src/Solution/StoryTesterImpl.java
@@ -2,6 +2,9 @@ package Solution;
 
 import Provided.StoryTester;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StoryTesterImpl implements StoryTester {
     @Override
     public void testOnInheritanceTree(String story, Class<?> testClass) throws Exception {
@@ -12,4 +15,47 @@ public class StoryTesterImpl implements StoryTester {
     public void testOnNestedClasses(String story, Class<?> testClass) throws Exception {
         //TODO: Implement
     }
+
+    static public class WhenThenStruct {
+        List<String> whens;
+        List<String> thens;
+
+        public WhenThenStruct(List<String> whens, List<String> thens) {
+            this.whens = whens;
+            this.thens = thens;
+        }
+    }
+
+    static public class StoryStruct{
+        String givenSentence;
+        List<WhenThenStruct> whenThenGroups;
+        public StoryStruct(String story) {
+            String[] lines = story.split("\\r?\\n");
+            givenSentence = lines[0];
+            whenThenGroups = new LinkedList<>();
+            boolean collectingWhen = true;
+            List<String> whens = new LinkedList<>();
+            List<String> thens = new LinkedList<>();
+            // skip Given and iterate over sentences
+            for (int i = 1; i < lines.length; i++) {
+                String line = lines[i];
+                if (line.startsWith("When")) {
+                    // if When came after Then, append the when-then group to the list
+                    if(!collectingWhen){
+                        whenThenGroups.add(new WhenThenStruct(whens, thens));
+                        whens = new LinkedList<>();
+                        thens = new LinkedList<>();
+                    }
+                    whens.add(line);
+                    collectingWhen = true;
+                } else if (line.startsWith("Then")) {
+                    thens.add(line);
+                    collectingWhen = false;
+                }
+            }
+            // add the last batch
+            whenThenGroups.add(new WhenThenStruct(whens, thens));
+        }
+    }
+
 }
