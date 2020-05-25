a418daa1b2efad5ce2e6a9f30c670d1847e520fb
Dor-B <dor46200@gmail.com>
2020-05-25 19:50:51 +0300

    Implement StoryTestException, Add copy of test object, Add testing a whens-thens group on a test object in testWhenThenOnObject(). Fix grammar problem in provided DogTest.java

---
+++ src/Provided/StoryTestExceptionImpl.java
@@ -0,0 +1,51 @@
+package Provided;
+
+import java.util.List;
+
+public class StoryTestExceptionImpl extends StoryTestException {
+    private int numFail;
+    private String sentence;
+    private List<String> storyExpected;
+    private List<String> testResult;
+
+    public StoryTestExceptionImpl(String sentence, List<String> storyExpected, List<String> testResult) {
+        this.sentence = sentence;
+        this.storyExpected = storyExpected;
+        this.testResult = testResult;
+    }
+
+    public void setNumFail(int numFail) {
+        this.numFail = numFail;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public void setStoryExpected(List<String> storyExpected) {
+        this.storyExpected = storyExpected;
+    }
+
+    public void setTestResult(List<String> testResult) {
+        this.testResult = testResult;
+    }
+    @Override
+    public String getSentence() {
+        return sentence;
+    }
+
+    @Override
+    public List<String> getStoryExpected() {
+        return storyExpected;
+    }
+
+    @Override
+    public List<String> getTestResult() {
+        return testResult;
+    }
+
+    @Override
+    public int getNumFail() {
+        return numFail;
+    }
+}
--- src/Solution/StoryTesterImpl.java
+++ src/Solution/StoryTesterImpl.java
@@ -1,12 +1,18 @@
 package Solution;
 
-import Provided.StoryTester;
+import Provided.*;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.LinkedList;
 import java.util.List;
+
 import Solution.AnnotaionsHelper;
+import org.junit.ComparisonFailure;
+
 enum annotationType{
     GIVEN,
     WHEN,
@@ -24,6 +30,102 @@ public class StoryTesterImpl implements StoryTester {
     @Override
     public void testOnNestedClasses(String story, Class<?> testClass) throws Exception {
         //TODO: Implement
+    }
+    public Object copyTestObject(Object testObj) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException{
+        Object backup = testObj.getClass().newInstance();
+        for(Field field : testObj.getClass().getFields()){
+            field.setAccessible(true);
+            Object fieldVal = field.get(testObj);
+            if(fieldVal instanceof Cloneable){
+                field.set(backup, fieldVal.getClass().getMethod("clone").invoke(fieldVal));
+            }else{
+                try{
+                    Constructor copyConstructor = fieldVal.getClass().getConstructor(fieldVal.getClass());
+                    field.set(backup, copyConstructor.newInstance(fieldVal));
+                }catch (NoSuchMethodException exp){
+                    field.set(backup, fieldVal);
+                }
+            }
+        }
+        return backup;
+    }
+    public static Object[] stringParamsToGeneral(List<String> params){
+        return params.stream()
+                .map(p -> (Object) p)
+                // cast to integer if necessary
+                .map(o -> ((String) o).matches("-?\\d+") ?
+                        Integer.parseInt((String) o) : o).toArray();
+    }
+    public void searchAndInvoke(Object testObj, String lineWithoutBeginning, annotationType annoType) throws WordNotFoundException {
+        Tuple<Method, List<String>> methodData = searchAnnotation(testObj.getClass(), lineWithoutBeginning, annotationType.WHEN);
+        if(methodData == null){
+            switch (annoType){
+                case THEN:
+                    throw new ThenNotFoundException();
+                case WHEN:
+                    throw new WhenNotFoundException();
+                case GIVEN:
+                    throw new GivenNotFoundException();
+            }
+        }
+        Method method = methodData.first;
+        Object[] params = stringParamsToGeneral(methodData.second);
+        try {
+            method.invoke(testObj, params);
+        }catch (InvocationTargetException ite){
+            if(ite.getCause() instanceof ComparisonFailure){
+                throw (ComparisonFailure) ite.getCause();
+            }
+        }catch (Exception e){ // should not be here
+            e.printStackTrace();
+        }
+    }
+    public void testWhenThenOnObject(Object testObj, WhenThenStruct whenThenStruct) throws WordNotFoundException, StoryTestExceptionImpl {
+        Object backup = null;
+        try{
+            backup = copyTestObject(testObj);
+        }catch (Exception e){ // should not be here
+            e.printStackTrace();
+            return;
+        }
+        for(String whenLine : whenThenStruct.whens){
+            whenLine = AnnotaionsHelper.removeFirstWord(whenLine);
+            searchAndInvoke(testObj, whenLine, annotationType.WHEN);
+        }
+        StoryTestExceptionImpl testException = null;
+        boolean noErrorsSoFar = true;
+        int numFails = 0;
+        for(String thenLine : whenThenStruct.thens){
+            String thenLineClean = AnnotaionsHelper.removeFirstWord(thenLine);
+            String[] orSeparated = AnnotaionsHelper.separateByOr(thenLineClean);
+            List<String> storyExpected = new LinkedList<>();
+            List<String> storyActual = new LinkedList<>();
+            boolean foundGood = false;
+            for(String thenPart : orSeparated){
+                try{
+                    searchAndInvoke(testObj, thenPart, annotationType.THEN);
+                    foundGood = true;
+                    break;
+                }catch (ComparisonFailure cmpFailErr){
+                    testObj = backup;
+                    if(noErrorsSoFar){
+                        storyExpected.add(cmpFailErr.getExpected());
+                        storyActual.add(cmpFailErr.getActual());
+                    }
+                }
+            }
+            if(!foundGood){ // all
+                if(noErrorsSoFar){ // this is the first then to fail
+                    noErrorsSoFar = false;
+                    testException = new StoryTestExceptionImpl(thenLine, storyExpected, storyActual);
+                }
+                numFails++;
+                testException.setNumFail(numFails);
+            }
+        }
+        if(testException != null){
+            throw testException;
+        }
     }
 
     static public class WhenThenStruct {
